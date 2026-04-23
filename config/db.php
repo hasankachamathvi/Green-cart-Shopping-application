e5a79f32e4b0a4d2fc22fa9aeb72ecfd93226c8b
hasankachamathvi <150053360+hasankachamathvi@users.noreply.github.com>
2026-04-23 14:58:54 +0530

    Refactor database connection code for improved readability

--- config/db.php
+++ config/db.php
@@ -1,14 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "shopping_cart_db";
+$username   = "root";
+$password   = "";
+$dbname     = "shopping_cart_db";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-?>
